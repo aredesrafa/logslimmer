b16e3166587acd000104757eaf03cb3d1b146599
Rafael Aredes <rafaelaredes@MacBook-Air-de-Rafael.local>
2025-11-15 18:14:10 -0300

    ci: add github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { sveltePreprocess } from 'svelte-preprocess'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = process.env.BASE_PATH ?? '/'
+
 export default defineConfig({
+  base,
   plugins: [svelte({ preprocess: sveltePreprocess() }), tailwindcss()],
   server: {
     port: 3000,
